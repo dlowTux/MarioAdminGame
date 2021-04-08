1685b3eba3954f7d27e66d264747117f65592fd7
dlowTux <omarlopez22520@gmail.com>
2021-04-08 00:00:54 -0500

    checking if the user chose the same team in clash

--- static/js/admint.js
+++ static/js/admint.js
@@ -20,3 +20,23 @@ if (btnstart) {
         });
     })
 }
+document
+    .getElementById("formclash")
+    .addEventListener("submit", function (e) {
+        e.preventDefault();
+        const log = document.querySelector("#errors");
+        log.innerHTML = " ";
+        const team1 = document.getElementById("txtteam1").value;
+        const score1 = document.getElementById("txtscore1").value;
+        const team2 = document.getElementById("txtteam2").value;
+        const score2 = document.getElementById("txtscore2").value;
+        if (team1 == team2) {
+            const label = document.createElement("label");
+            label.innerText =
+                "Error you can not select the same teams";
+            label.classList.add("error");
+            log.append(label);
+        } else {
+            console.log('Diferentes')
+        }
+    });
